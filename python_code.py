8edcb2a669321b6ab0b22c04fca332d5c1ecdb5a
kelvinxu1989 <62630385+kelvinxu1989@users.noreply.github.com>
2020-03-25 10:39:58 -0700

    Add files via upload

--- python_code.py
+++ python_code.py
@@ -1,3 +1,13 @@
 # github-example
 
 print("Hello Github")
+
+
+Description
+This notebook will be mainly used for the capstone project.
+
+import pandas as pd
+import numpy as np
+
+print("Hello Caspstone Projects Course!")
+
