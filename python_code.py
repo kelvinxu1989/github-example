6fac467ba76eff13715ffabb1e42a61a76f663c5
kelvinxu1989 <62630385+kelvinxu1989@users.noreply.github.com>
2020-03-25 10:42:24 -0700

    Update python_code.py

--- python_code.py
+++ python_code.py
@@ -3,11 +3,4 @@
 print("Hello Github")
 
 
-Description
-This notebook will be mainly used for the capstone project.
-
-import pandas as pd
-import numpy as np
-
-print("Hello Caspstone Projects Course!")
 
